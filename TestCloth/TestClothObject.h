30978c14b1b940c2e5fee6afc515b554581d1cd9
massanoori-home <shinkawa.masanori@gmail.com>
2014-07-01 19:31:14 +0900

    controllable cloth simulation parameter

--- TestCloth/TestClothObject.h
+++ TestCloth/TestClothObject.h
@@ -2,4 +2,21 @@
 
 #include "ObjectList.h"
 
-ObjectHandle CreateTestClothObject();
+namespace TestCloth
+{
+	struct Spring
+	{
+		float Stiffness;
+		float Damping;
+	};
+
+	struct Desc
+	{
+		Spring Neighbour = Spring{ 100000.0f, 30.0f };
+		Spring Diagonal = Spring{ 100000.0f, 30.0f };
+		Spring Bending = Spring{ 400000.0f, 20.0f };
+		float TimeStep = 0.001f;
+	};
+
+	ObjectHandle CreateObject(const Desc& desc);
+}
